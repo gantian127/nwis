e452d11302655a9a3d379d02c2e6f8900b15125e
gantian127 <jamy127@foxmail.com>
2022-08-24 16:40:28 -0600

    nwis: update Nwis class with new methods

--- nwis/nwis.py
+++ nwis/nwis.py
@@ -1,107 +1,177 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
+
+import requests
+import pandas as pd
+from io import StringIO
 import dataretrieval.nwis as nwis
 
 
 class Nwis:
     def __init__(self):
         self._dataset = None
+        self._variable_info = None
+        self._site_info = None
 
     @property
-    def dataset(self):
+    def data(self):
         return self._dataset
 
-    def get_data(self, site, start_date, end_date, data_type='iv', nc_output=None):
+    @property
+    def variables(self):
+        return self._variable_info
+
+    @property
+    def sites(self):
+        return self._site_info
+
+    def get_data(self, sites, start, end, service, parameterCd=None, output=None, **kwargs):
 
         # check user input
-        Nwis._check_user_input(site, start_date, end_date, data_type, nc_output)
+        Nwis._check_user_input(sites, start, end, service, output)
 
         # get data
-        xr_dataset = Nwis._get_nwis_data(site, start_date, end_date, data_type, nc_output)
-        self._dataset = xr_dataset
+        nwis_data = Nwis._get_nwis_data(self, sites=sites, start=start, end=end, service=service,
+                                        parameterCd=parameterCd, output=output, **kwargs)
+        self._dataset = nwis_data
 
-        return xr_dataset
+        return nwis_data
 
     @staticmethod
-    def _check_user_input(site, start_date, end_date, data_type, nc_output):
-        # check site
-        try:
-            site_info = nwis.get_record(sites=site, service='site')
-            if site_info.empty:
+    def _check_user_input(sites, start, end, service, output):
+        # check sites
+        if type(sites) is str:
+            sites = [sites]
+
+        for site in sites:
+            try:
+                site_info = nwis.get_record(sites=site, service='site')
+                if site_info.empty:
+                    raise ValueError("Incorrect USGS site number.")
+            except Exception:
                 raise ValueError("Incorrect USGS site number.")
-        except Exception:
-            raise ValueError("Incorrect USGS site number.")
 
-        # check data_type
-        if data_type not in ['dv', 'iv']:
-            raise ValueError("Incorrect data type: 'dv' as daily value or 'iv' as instantaneous value {} ".format(data_type))
-
-        # check time
+        # check start end time
         try:
-            start_datetime = datetime.strptime(start_date, '%Y-%m-%d')
-            end_datetime = datetime.strptime(end_date, '%Y-%m-%d')
+            start_datetime = datetime.strptime(start, '%Y-%m-%d')
+            end_datetime = datetime.strptime(end, '%Y-%m-%d')
 
             if start_datetime > end_datetime:
                 raise ValueError('Incorrect start date.')
 
         except Exception:
-            raise ValueError("Incorrect date format, should be YYYY-MM-DD")
+            raise ValueError("Incorrect date format. It needs to be YYYY-MM-DD. ")
 
-        # check csv_output
-        if nc_output and nc_output[-3:] != '.nc':
-            raise ValueError('Incorrect NetCDF file path.')
+        # check service
+        if service not in ['dv', 'iv']:
+            raise ValueError("Incorrect service option '{}': 'dv' as daily mean data or 'iv' as instantaneous data.".format(service))
 
-    @staticmethod
-    def _get_nwis_data(site, start_date, end_date, data_type, nc_output):
+        # check output
+        if output and output[-3:] != '.nc':
+            raise ValueError('Incorrect NetCDF file path for the output.')
 
-        variable_info = {
-            '00060': ['discharge', 'cubic feet per second'],
-            '00065': ['gage height', 'feet'],
-            '00010': ['water temperature', 'degree celsius'],
-            '80154': ['Suspended sediment discharge', 'tons per day'],
-            '80155': ['Total sediment discharge', 'tons per day'],
-            '80225': ['Bedload sediment discharge', 'tons per day']}
-
-        # get site info
-        site_info = nwis.get_record(sites=site, service='site')
+    def _get_nwis_data(self, sites, start, end, service, parameterCd=None, output=None, **kwargs):
 
         # get time series data frame
-        record_df = nwis.get_record(sites=site, service=data_type, start=start_date, end=end_date)
-        filter_names = list(variable_info.keys()) + [var_name+'_Mean' for var_name in variable_info.keys()]
-        var_col_names = [col_name for col_name in record_df.columns if col_name in filter_names]
+        ts_df = nwis.get_record(sites=sites, service=service, start=start, end=end, parameterCd=parameterCd, **kwargs)
 
-        if record_df.empty or not var_col_names:
-            raise ValueError('Time series for discharge variables is not available for site {}.'.format(site))
+        if ts_df.empty:
+            print('Data is not available for the provided parameters.')
+            raise
+        else:
+            # get site info
+            site_info = Nwis._get_site_info(sites=sites)    # sites=['03339000','01542500']
+            self._site_info = site_info
 
-        time_series_df = record_df[var_col_names]
-        time_series_df.columns = [col_name[:5] for col_name in time_series_df.columns]
+            # get variable info
+            parameterCd = list(set([name.split('_')[0] for name in ts_df.columns if name.split('_')[0].isnumeric()]))  # parameterCd = ['00060','00095']
+            variable_info = Nwis._get_variable_info(parameterCd)
+            self._variable_info = variable_info
 
-        # create xarray dataset
-        xr_dataset = time_series_df.to_xarray()
+            # refine data frame
+            filter_names = []
+            for code in parameterCd:
+                if code in ts_df.columns:
+                    filter_names.append(code)
+                elif code+'_Mean' in ts_df.columns:
+                    filter_names.append(code+'_Mean')
 
-        # assign datetime data to coordinate
-        xr_dataset['datetime'] = time_series_df.index.values
+            nwis_df = ts_df[filter_names]  # this removes the max, min, cd columns from the dataframe
+            new_col_names = [name.split('_')[0] for name in filter_names]
+            nwis_df.columns = new_col_names
 
-        # add site metadata
-        xr_dataset.attrs['site_name'] = site_info.station_nm[0]
-        xr_dataset.attrs['site_code'] = site_info.site_no[0]
-        xr_dataset.attrs['site_latitude'] = site_info.dec_lat_va[0]
-        xr_dataset.attrs['site_longitude'] = site_info.dec_long_va[0]
-        xr_dataset.attrs['site_altitude'] = site_info.alt_va[0]
-        xr_dataset.attrs['site_coord_datum'] = site_info.dec_coord_datum_cd[0]
+            # convert to xarray
+            nwis_data = nwis_df.to_xarray()  # in the array, the row is site number, column is time step
+            nwis_data.coords['datetime'] = nwis_df.index.levels[nwis_df.index.names.index('datetime')].values
 
-        # add variable metadata
-        for var_name in time_series_df.columns:
-            xr_dataset[var_name].attrs['variable_name'] = variable_info[var_name][0]
-            xr_dataset[var_name].attrs['variable_unit'] = variable_info[var_name][1]
-            xr_dataset[var_name].attrs['variable_data_type'] = data_type if data_type == 'dv' 'daily value' \
-                else 'instantaneous value'
+            # save output file as csv file
+            if output:
+                try:
+                    nwis_data.to_netcdf(output)
+                except Exception:
+                    print('Failed to write the data in the NetCDF file.')
+                    raise
 
-        # save output file as csv file
-        if nc_output:
+        return nwis_data
+
+    @staticmethod
+    def _get_site_info(sites):
+        site_info = {}
+        site_info_df = nwis.get_record(sites=sites, service='site')
+        for index, row in site_info_df.iterrows():
+            site_info[row['site_no']] = {
+                'site_lat': row['dec_lat_va'],
+                'site_lon': row['dec_long_va'],
+                'site_altitude': row['alt_va'],
+                'site_name': row['station_nm'],
+            }
+
+        return site_info
+
+    @staticmethod
+    def _get_variable_info(parameterCd):
+        variable_info = {}
+        parameterCd = [parameterCd] if type(parameterCd) is str else parameterCd
+        print(parameterCd)
+
+        for parameter in parameterCd:
             try:
-                xr_dataset.to_netcdf(nc_output)
-            except Exception:
-                print('Failed to write the data in the NetCDF file.')
+                url = 'https://help.waterdata.usgs.gov/code/parameter_cd_nm_query?' \
+                      'parm_nm_cd={}&fmt=rdb'.format(parameter)
+                response = requests.get(url)
 
-        return xr_dataset
+                headerlines = []
+                datalines = []
+                count = 0
+
+                text = response.text
+                for line in text.splitlines():
+                    if line[0] == "#":
+                        headerlines.append(line)
+                    elif count == 0:
+                        columns = line.split()
+                        count += 1
+                    elif count == 1:
+                        types = line.split()
+                        count += 1
+                    else:
+                        datalines.append(line)
+
+                data = "\n".join(datalines)
+
+                variable_info_df = pd.read_csv(
+                    StringIO(data),
+                    sep="\t",
+                    comment="#",
+                    header=None,
+                    names=columns,
+                    dtype={"parameter_cd": str},
+                )
+
+            except Exception as e:
+                print('Failed to get the variable information from the NWIS system.')
+                raise
+
+            variable_info[parameter] = [variable_info_df['SRSName'][0].split(',')[0], variable_info_df['parm_unit'][0]]
+
+        return variable_info
